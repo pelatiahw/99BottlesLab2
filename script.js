123d45f9be0145ca538baa35617b871e6ba2eff7
InnovationBham_4 <InnovationBham_4@Admins-iMac.local>
2017-06-19 11:04:21 -0500

    inital commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var friends = ['steve', 'bob', 'susan', 'rose', 'kanye'];
+
+    for(var i = 0; i<friends.length; i++) {
+    for(var j = 99; j > 0; j--) {
+        if (j < 0) {
+            console.log(j + ' lines of code in the file,'+j+ ' lines of code;'+friends[i]+' strikes one out, clears it all out,'+j+' lines of code in the file'+j+' lines of code in the file,'+j+'lines of code;'+friends[i]+' strikes one out,clears it all out,'+j+' lines of code in the file.');
+        }
+        else if (j > 0) {
+            console.log(j +' line of code in the file,'+j+ ' lines of code;'+friends[i]+' strikes one out, clears it all out,'+j+' line of code in the file'+j+' line of code in the file,'+j+'line of code;'+friends[i]+' strikes one out,clears it all out,'+j+' no more lines of code in the file.');
+        }
+
+    };
+};
