a4fa7fad2288daefe8616151526fb2fdd6f6f9fd
InnovationBham_4 <InnovationBham_4@Admins-iMac.local>
2017-06-19 14:10:16 -0500

    commit

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-var friends = ['steve', 'bob', 'susan', 'rose', 'kanye'];
+var btn = document.getElementById('my-btn');
+btn.addEventListener('click', function() {
+   var friends = ['steve', 'bob', 'susan', 'rose', 'kanye'];
 
     for(var i = 0; i<friends.length; i++) {
     for(var j = 99; j > 0; j--) {
         if (j < 0) {
             console.log(j + ' lines of code in the file,'+j+ ' lines of code;'+friends[i]+' strikes one out, clears it all out,'+j+' lines of code in the file'+j+' lines of code in the file,'+j+'lines of code;'+friends[i]+' strikes one out,clears it all out,'+j+' lines of code in the file.');
         }
-        else if (j === 2) {
+        else if (j == 2) {
+            console.log(j +' lines of code in the file,'+j+ ' lines of code;'+friends[i]+' strikes one out, clears it all out,'+j+' lines of code in the file'+j+' lines of code in the file,'+j+'line of code;'+friends[i]+' strikes one out,clears it all out,'+j+' lines of code in the file.');
+        }
+        else {
             console.log(j +' line of code in the file,'+j+ ' lines of code;'+friends[i]+' strikes one out, clears it all out,'+j+' line of code in the file'+j+' line of code in the file,'+j+'line of code;'+friends[i]+' strikes one out,clears it all out,'+j+' no more lines of code in the file.');
         }
-
     };
 };
-var btn = document.getElementById("Button");
-btn.addEventListener("click", sing);
+});
