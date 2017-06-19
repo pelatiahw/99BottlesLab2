e301a35696030cd0d3da0c69ed9395c36805ff0b
InnovationBham_4 <InnovationBham_4@Admins-iMac.local>
2017-06-19 16:25:43 -0500

    lab2

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
-var btn = document.getElementById('my-btn');
-btn.addEventListener('click', function() {
-   var friends = ['steve', 'bob', 'susan', 'rose', 'kanye'];
+var friends = ['steve', 'bob', 'susan', 'rose', 'kanye'];
+        function singing () {
 
-    for(var i = 0; i<friends.length; i++) {
-    for(var j = 99; j > 0; j--) {
-        if (j < 0) {
-            console.log(j + ' lines of code in the file,'+j+ ' lines of code;'+friends[i]+' strikes one out, clears it all out,'+j+' lines of code in the file'+j+' lines of code in the file,'+j+'lines of code;'+friends[i]+' strikes one out,clears it all out,'+j+' lines of code in the file.');
-        }
-        else if (j == 2) {
-            console.log(j +' lines of code in the file,'+j+ ' lines of code;'+friends[i]+' strikes one out, clears it all out,'+j+' lines of code in the file'+j+' lines of code in the file,'+j+'line of code;'+friends[i]+' strikes one out,clears it all out,'+j+' lines of code in the file.');
-        }
-        else {
-            console.log(j +' line of code in the file,'+j+ ' lines of code;'+friends[i]+' strikes one out, clears it all out,'+j+' line of code in the file'+j+' line of code in the file,'+j+'line of code;'+friends[i]+' strikes one out,clears it all out,'+j+' no more lines of code in the file.');
-        }
+
+              for(var i = 0; i<friends.length; i++) {
+                    var div = document.createElement('div')
+                    var h3 = document.createElement('h3')
+                    div.className = 'friends';
+                    h3.innerHTML = friends[i];
+                    document.body.appendChild(div);
+                    div.appendChild(h3);
+                    var para = document.createElement("p");
+                    div.appendChild(para);
+
+              for(var j = 99; j > 0; j--) {
+                    if (j < 0) {
+                        console.log(j + ' lines of code in the file,'+j+ ' lines of code;'+friends[i]+' strikes one out, clears it all out,'+j+' lines of code in the file'+j+' lines of code in the file,'+j+'lines of code;'+friends[i]+' strikes one out,clears it all out,'+j+' lines of code in the file.');
+                    }
+                    else if (j == 2) {
+                        console.log(j +' lines of code in the file,'+j+ ' lines of code;'+friends[i]+' strikes one out, clears it all out,'+j+' lines of code in the file'+j+' lines of code in the file,'+j+'line of code;'+friends[i]+' strikes one out,clears it all out,'+j+' lines of code in the file.');
+                    }
+                    else {
+                        console.log(j +' line of code in the file,'+j+ ' lines of code;'+friends[i]+' strikes one out, clears it all out,'+j+' line of code in the file'+j+' line of code in the file,'+j+'line of code;'+friends[i]+' strikes one out,clears it all out,'+j+' no more lines of code in the file.');
+                }
+            
+        };
     };
+    document.body.appendChild(div);
 };
-});
+var btn = document.getElementById('my-btn');
+btn.addEventListener('click', singing)
